96a614f844a4fb6b96c1aa3d90c0fea0f1b50cd1
quorynth <quorynth@protonmail.com>
2025-08-20 13:17:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',           // ← головне, щоб Next зібрав статичний сайт у /out
+  images: { unoptimized: true }
+}
 module.exports = nextConfig
