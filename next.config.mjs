34d83d2ba0c8d56cd9c2fa51315dfa09f41994a6
quorinth <quorynth@protonmail.com>
2025-08-22 21:16:58 +0300

    update 1.2.8

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,27 @@
 // next.config.mjs
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
           {
-            key: "Content-Security-Policy",
+            key: 'Content-Security-Policy',
             value: `
               default-src 'self';
               base-uri 'self';
               frame-ancestors 'self';
+              object-src 'none';
               script-src 'self' 'unsafe-inline';
               style-src 'self' 'unsafe-inline';
-              img-src 'self' data: blob: https://cdn.tldraw.com;
+              img-src 'self' data: blob: https://cdn.tldraw.com https://*.tldraw.com;
               font-src 'self' https://cdn.tldraw.com data:;
               connect-src 'self' blob: https://cdn.tldraw.com https://polygon-mainnet.g.alchemy.com https://*.walletconnect.com https://*.reown.com;
               worker-src 'self' blob:;
-              frame-src 'self' https://*.walletconnect.com;
+              frame-src 'self' https://*.walletconnect.com https://cdn.tldraw.com;
+              child-src 'self' https://*.walletconnect.com https://cdn.tldraw.com;
               manifest-src 'self' https://cdn.tldraw.com;
-            `.replace(/\s{2,}/g, ' ').trim(),
+            `.replace(/\\s{2,}/g, ' ').trim(),
           },
         ],
       },
